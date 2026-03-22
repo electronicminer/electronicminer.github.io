afa693406b1ac63ea06bb82b7f8ca9bf156460d7
electronicminer <161852542+electronicminer@users.noreply.github.com>
2026-03-22 13:51:33 +0000

    Deploying to gh-pages from @ electronicminer/electronicminer.github.io@75f655219522a9ec5e6a17ca216ea6dfa02a79d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-关于我-about",
-    title: "关于我 (About)",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
