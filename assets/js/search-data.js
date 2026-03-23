92f09b728a1626469a5b912f128cd692e2f3f13e
electronicminer <161852542+electronicminer@users.noreply.github.com>
2026-03-23 06:00:16 +0000

    Deploying to gh-pages from @ electronicminer/electronicminer.github.io@f2096c89e80d5cbb6d4ec956df3fbf7a275e610b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,7 +507,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%61%6E%67%70%61%6E%30%34%30%39%33%30@%31%36%33.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -529,13 +529,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
